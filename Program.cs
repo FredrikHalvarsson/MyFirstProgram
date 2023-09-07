754269426a900ec4f013d637aa671d93ab12cda4
Fredr <Fredr@FREDDO>
2023-09-07 10:18:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MyFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 15;
+            if (num > 10)
+            {
+                Console.WriteLine("Talet är stort!");
+            }
+            else
+            {
+                Console.WriteLine("Oj, Lågt tal!");
+            }
+
+            Console.WriteLine("Vad heter du?");
+            string namn = Console.ReadLine();
+            Console.WriteLine($"Hej {namn}!");
+
+            for (int i = 0; i <= num; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
